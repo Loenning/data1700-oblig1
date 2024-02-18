c67b35102ec0250d657513a0a86c99999c049d43
Thomas Stavik Lønning <thlon2720@oslomet.no>
2024-02-18 18:00:24 +0100

    Opprettelse av Spring Boot-prosjekt og oppdatering av README-filen.

--- src/main/resources/static/oblig1.js
+++ src/main/resources/static/oblig1.js
@@ -1,5 +1,5 @@
 //------Creating an empty array for ticket orders
-const tickets = []
+const ticketsArray = []
 
 //------function for buying tickets
 function buyTicket() {
@@ -9,6 +9,7 @@ function buyTicket() {
     const etternavn = document.getElementById("etternavn");
     const telefonnr = document.getElementById("telefonnr");
     const epost = document.getElementById("epost");
+    const wrongFilm = document.getElementById("wrongFilm")
     const wrongAmount = document.getElementById("wrongAmount");
     const wrongFirstName = document.getElementById("wrongFirstName");
     const wrongLastName = document.getElementById("wrongLastName");
@@ -17,30 +18,30 @@ function buyTicket() {
 
     let wrongInput = false;
 
-//------ if-statements that validate the user input using regex,
-//------ Using the wrongInput variable to hinder an order with invalid inputs from being put into the tickets array
-
-    if (!/^[1-99]+$/.test(antall.value)) {
-        wrongAmount.innerText = "Velg antall billetter";
+//------ Function to validate the user input and gives different messages depending on if the input is missing
+//------ or not matching the given regex parameter
+    function validateInput(value, regex, errorElement, errorEmpty, errorRegex) {
+        if (regex.test(value)) {
+            errorElement.innerText = "";
+            return;
+        }
         wrongInput = true;
-    }
-    if (!/^[a-åA-Å]+$/.test(fornavn.value)) {
-        wrongFirstName.innerText = "Skriv inn fornavn";
-        wrongInput = true;
-    }
-    if (!/^[a-åA-Å]+$/.test(etternavn.value)) {
-        wrongLastName.innerText = "Skriv inn etternavn";
-        wrongInput = true;
-    }
-    if (!/^\d{8}$/.test(telefonnr.value)) {
-        wrongNumber.innerText = "Skriv inn ett gyldig telefonnummer (8 tall)";
-        wrongInput = true;
-    }
-    if (!/^[a-åA-Å0-9._%+-]+@[a-åA-Å0-9.-]+\.[a-åA-Å]+$/.test(epost.value)) {
-        wrongEmail.innerText = "Skriv inn epost";
-        wrongInput = true;
+        if (!value) {
+            errorElement.innerText = errorEmpty;
+            return;
+        }
+        errorElement.innerText = errorRegex;
     }
 
+//------ Validating input for each of the boxes, using parameters for each of the variables
+    validateInput(film.value, /^[^]+$/, wrongFilm, "Velg film", "Velg film")
+    validateInput(antall.value, /^[1-99]+$/, wrongAmount, "Skriv inn antall", "Skriv inn gyldig antall")
+    validateInput(fornavn.value, /^[a-zæøåA-ÅÆØÅ]+$/, wrongFirstName, "Skriv inn fornavn", "Skriv inn gyldig fornavn")
+    validateInput(etternavn.value, /^[a-zæøåA-ZÆØÅ]+$/, wrongLastName, "Skriv inn etternavn", "Skriv inn gyldig etternavn")
+    validateInput(telefonnr.value, /^\d{8}$/, wrongNumber, "Skriv inn telefonnr", "Skriv inn gyldig telefonnr (8 tall)")
+    validateInput(epost.value, /^[a-zæøåA-ZÆØÅ0-9._%+-]+@[a-zæøåA-ZÆØÅ0-9.-]+\.[a-zæøåA-ZÆØÅ]+$/, wrongEmail, "Skriv inn epost", "Skriv inn gyldig epost")
+
+//------ If the input validation function doesn't change the value of the wrongInput variable, an order is created
     if (wrongInput === false) {
         const order = {
             film: film.value,
@@ -50,31 +51,17 @@ function buyTicket() {
             telefonnr: telefonnr.value,
             epost: epost.value
         };
+
 //------ Puts the created order into the tickets array
-        tickets.push(order);
+        ticketsArray.push(order);
+
 //------ Resetting the variable values once the order is pushed
         film.value = "";
         antall.value = "";
-        antall.innerText = "";
         fornavn.value = "";
-        fornavn.innerText = "";
         etternavn.value = "";
-        etternavn.innerText = "";
         telefonnr.value = "";
-        telefonnr.innerText = "";
         epost.value = "";
-        epost.innerText = "";
-
-        wrongAmount.value = "";
-        wrongAmount.innerText = "";
-        wrongFirstName.value = "";
-        wrongFirstName.innerText = "";
-        wrongLastName.value = "";
-        wrongLastName.innerText = "";
-        wrongNumber.value = "";
-        wrongNumber.innerText = "";
-        wrongEmail.value = "";
-        wrongEmail.innerText = "";
     }
 
 //------ Function for printing out the tickets array
@@ -83,7 +70,7 @@ function buyTicket() {
         let ut = "<table id='table'><tr>" +
             "<th>Film</th><th>Antall</th><th>Navn</th><th>Etternavn</th><th>Telefonnr</th><th>Epost</th>" +
             "</tr>";
-        for (let i of tickets) {
+        for (let i of ticketsArray) {
             ut += "<tr>";
             ut += "<td>" + i.film + "</td><td>" + i.antall + "</td><td>"
                 + i.fornavn + "</td><td>" + i.etternavn + "</td><td>"
